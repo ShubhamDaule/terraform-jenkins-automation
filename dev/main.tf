8b524e8a96c91b4ae0d46ca677344a8c53410ec9
unknown <shubhamdaule@gmail.com>
2023-05-02 13:12:34 -0500

    test

--- dev/main.tf
+++ dev/main.tf
@@ -8,3 +8,32 @@ resource "aws_instance" "myFirstInstance" {
     Name = "myFirstInstance"
   }
 }
+
+resource "aws_security_group" "launch-wizard-1" {
+  name        = "launch-wizard-1"
+  description = "launch-wizard-1 created 2023-04-27T16:34:58.265Z"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
+
+  ingress{
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
+}
